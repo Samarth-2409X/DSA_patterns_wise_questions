181142a7ad8f4b50d469421a6410cecc59039194
Samarth-2409X <samarthgone667@gmail.com>
2026-05-03 09:56:16 +0530

    stack

---
+++ Stack/Valid Parentheses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<int> st;
+        
+        for(char &ch: s){
+            if(st.empty() || ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+                continue;
+            }
+            
+            if(ch == ')'){
+                if(st.top() == '('){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+
+            if(ch == '}'){
+                if(st.top() == '{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+
+            if(ch == ']'){
+                if(st.top() == '['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
