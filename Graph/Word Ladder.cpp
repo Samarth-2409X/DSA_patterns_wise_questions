388643c50d5017079a9283dd13655f300bd0e7d1
Samarth-2409X <samarthgone667@gmail.com>
2026-08-29 23:21:05 +0530

    Graph

---
+++ Graph/Word Ladder.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string, int>> que;
+        que.push({beginWord, 1});
+
+        unordered_set<string> st(begin(wordList), end(wordList));
+        st.erase(beginWord);
+
+        while(!que.empty()){
+            string word = que.front().first;
+            int steps = que.front().second;
+            que.pop();
+            
+            if(word == endWord){
+                return steps;
+            }
+
+            for(int i=0; i<word.size(); i++){
+                char original = word[i];
+                for(char ch = 'a'; ch <= 'z'; ch++){
+                    word[i] = ch;
+                    if(st.find(word) != st.end()){
+                        st.erase(word);
+                        que.push({word, steps + 1});
+                    }
+                }
+                word[i] = original;
+            }
+        }
+
+        return 0;
+    }
+};
